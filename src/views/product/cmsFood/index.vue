a331ec19503e79a54802edc730b40226fc2a732c
wenxiaojie <wxj_6699@163.com>
2024-03-22 23:17:19 +0800

    提交前端有商品可以update的

--- src/views/product/cmsFood/index.vue
+++ src/views/product/cmsFood/index.vue
@@ -4,10 +4,10 @@
     <div class="head-container">
       <div v-if="crud.props.searchToggle">
         <!-- 搜索 -->
-        <label class="el-form-item-label">食品名称</label>
-        <el-input v-model="query.foodName" clearable placeholder="食品名称" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <label class="el-form-item-label">食品类型</label>
-        <el-input v-model="query.foodType" clearable placeholder="食品类型" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">商品名称</label>
+        <el-input v-model="query.foodName" clearable placeholder="商品名称" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+        <label class="el-form-item-label">商品类型</label>
+        <el-input v-model="query.foodType" clearable placeholder="商品类型" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
         <label class="el-form-item-label">供应商</label>
         <el-input v-model="query.supplier" clearable placeholder="供应商" style="width: 185px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
         <label class="el-form-item-label">存储条件</label>
@@ -43,24 +43,19 @@
       <!--表单组件-->
       <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
         <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
-          <el-form-item label="食品名称" prop="foodName">
+          <el-form-item label="商品名称" prop="foodName">
             <el-input v-model="form.foodName" style="width: 370px;" />
           </el-form-item>
-          <el-form-item v-if="crud.status.add" label="食品图片">
-            <el-upload
-              ref="upload"
-              :limit="1"
-              :before-upload="beforeUpload"
-              :auto-upload="false"
-              :headers="headers"
-              :on-success="handleSuccess"
-              :on-error="handleError"
-              :action="fileUploadApi + '?name=' + form.name"
-            >
-              <div class="eladmin-upload"><i class="el-icon-upload" /> 添加图片</div>
-            </el-upload>
+          <el-form-item label="商品图：" prop="photoPath">
+            <el-button type="primary" @click="uploadPicture">上传图片</el-button>
+            <div v-if="form.photoPath" class="picture">
+              <a :href="form.photoPath" target="_blank">
+                <img :src="form.photoPath" class="uploaded-image">
+              </a>
+              <el-button class="del" type="danger" @click="deletePicture">删除</el-button>
+            </div>
           </el-form-item>
-          <el-form-item label="食品类型" prop="foodType">
+          <el-form-item label="商品类型" prop="foodType">
             <el-radio v-for="item in dict.food_type" :key="item.id" v-model="form.foodType" :label="item.value">{{ item.label }}</el-radio>
           </el-form-item>
           <el-form-item label="价格" prop="price">
@@ -93,12 +88,11 @@
       <!--表格渲染-->
       <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
         <el-table-column type="selection" width="55" />
-        <el-table-column prop="foodId" label="食品ID" />
-        <el-table-column prop="foodName" label="食品名称" />
+        <el-table-column prop="foodName" label="商品名称" />
         <!--
         <el-table-column prop="description" label="描述" />
 -->
-        <el-table-column prop="imgUrl" label="食品图片" width="150">
+        <el-table-column prop="imgUrl" label="商品图片" width="150">
           <template slot-scope="scope">
             <el-image
               style="width: 100px; height: 100px"
@@ -108,7 +102,7 @@
             />
           </template>
         </el-table-column>
-        <el-table-column prop="foodType" label="食品类型">
+        <el-table-column prop="foodType" label="商品类型">
           <template slot-scope="scope">
             {{ dict.label.food_type[scope.row.foodType] }}
           </template>
@@ -150,6 +144,7 @@ import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
+import axios from 'axios'
 
 const defaultForm = { foodId: null, foodName: null, description: null, foodType: null, price: null, supplier: null, productionDate: null, expiryDate: null, storageConditions: null, unit: null, stockQuantity: null, photoPath: null, createdAt: null, updatedAt: null }
 export default {
@@ -158,16 +153,16 @@ export default {
   mixins: [presenter(), header(), form(defaultForm), crud()],
   dicts: ['food_type', 'food_supplier', 'unit_type'],
   cruds() {
-    return CRUD({ title: '食品', url: 'api/cmsFood', idField: 'foodId', sort: 'foodId,desc', crudMethod: { ...crudCmsFood }})
+    return CRUD({ title: '商品', url: 'api/cmsFood', idField: 'foodId', sort: 'foodId,desc', crudMethod: { ...crudCmsFood }})
   },
   data() {
     return {
       rules: {
         foodName: [
-          { required: true, message: '食品名称不能为空', trigger: 'blur' }
+          { required: true, message: '商品名称不能为空', trigger: 'blur' }
         ],
         foodType: [
-          { required: true, message: '食品类型不能为空', trigger: 'blur' }
+          { required: true, message: '商品类型不能为空', trigger: 'blur' }
         ],
         price: [
           { required: true, message: '价格不能为空', trigger: 'blur' }
@@ -177,8 +172,8 @@ export default {
         ]
       },
       queryTypeOptions: [
-        { key: 'foodName', display_name: '食品名称' },
-        { key: 'foodType', display_name: '食品类型' },
+        { key: 'foodName', display_name: '商品名称' },
+        { key: 'foodType', display_name: '商品类型' },
         { key: 'price', display_name: '价格' },
         { key: 'supplier', display_name: '供应商' },
         { key: 'storageConditions', display_name: '存储条件' },
@@ -187,46 +182,57 @@ export default {
       ]
     }
   },
+  computed: {
+    handlePicture() {
+      const list = this.form.photoPath
+      const arr = []
+      for (let i = 0; i < list.length; i++) {
+        const item = list[i]
+        if (typeof item === 'string') arr.push(item)
+        else arr.push(URL.createObjectURL(item))
+      }
+      return arr
+    }
+  },
   methods: {
     // 钩子：在获取表格数据之前执行，false 则代表不获取数据
     [CRUD.HOOK.beforeRefresh]() {
       return true
     },
-    // 上传文件
-    upload() {
-      this.$refs.upload.submit()
-    },
-    beforeUpload(file) {
-      let isLt2M = true
-      isLt2M = file.size / 1024 / 1024 < 100
-      if (!isLt2M) {
-        this.loading = false
-        this.$message.error('上传文件大小不能超过 100MB!')
+    // 上传图片
+    uploadPicture() {
+      var input = document.createElement('input')
+      input.type = 'file'
+      input.accept = '.gif,.jpg,.jpeg,.bmp,.png,.GIF,.JPG,.PNG,.BMP'
+      input.onchange = (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append('file', file)
+
+        // 发送图片上传请求
+        axios.post('http://localhost:8000/api/localStorage/picturesV2', formData)
+          .then(response => {
+            this.form.photoPath = response.data.path // 假设后端返回的字段名为imageUrl
+          })
+          .catch(error => {
+            console.error('上传失败:', error)
+          })
       }
-      this.form.name = file.name
-      return isLt2M
+      input.click()
     },
-    handleSuccess(response, file, fileList) {
-      this.crud.notify('上传成功', CRUD.NOTIFICATION_TYPE.SUCCESS)
-      this.$refs.upload.clearFiles()
-      this.crud.status.add = CRUD.STATUS.NORMAL
-      this.crud.resetForm()
-      this.crud.toQuery()
-    },
-    // 监听上传失败
-    handleError(e, file, fileList) {
-      const msg = JSON.parse(e.message)
-      this.$notify({
-        title: msg.message,
-        type: 'error',
-        duration: 2500
-      })
-      this.loading = false
+    // 删除图片
+    deletePicture() {
+      this.form.photoPath = null
     }
   }
 }
 </script>
 
 <style scoped>
+.uploaded-image {
+  max-width: 200px; /* 设置图片的最大宽度 */
+  max-height: 200px; /* 设置图片的最大高度 */
+}
 
 </style>
+
